1693fe25000c2dba3f5dca7c260d6a9ef5c94027
chilmi89 <achmadchilmi87@gmail.com>
2025-02-12 18:49:49 +0700

    perbaikan halaman homepages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,8 @@
     <meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate">
     <meta http-equiv="Pragma" content="no-cache">
     <meta http-equiv="Expires" content="0">
-    <title>Login</title>
+    <title>HomePages
+    </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
